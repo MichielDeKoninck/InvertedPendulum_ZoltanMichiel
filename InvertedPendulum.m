ba8182ded1d3ffd84b5d8ba518e2d24e8fb6b7ca
Michiel De Koninck <dekoninckmichiel.mdk@gmail.com>
2019-05-13 14:24:33 +0200

    Update InvertedPendulum.m

--- InvertedPendulum.m
+++ InvertedPendulum.m
@@ -5,7 +5,7 @@ Km = 0.00767;
 Kb = 0.00767;
 Kg = 3.7;
 Mc = 0.455;
-l= 0.302;
+l= 0.305;
 m = 0.210;
 r = 0.00635;
 g= 9.81;
@@ -15,29 +15,32 @@ A = [0 0 1 0; 0 0 0 1; 0 -m*g/Mc -Kg^2*Km*Kb/(Mc*Rm*r^2) 0; 0 (Mc+m)*g/(Mc*l) Kg
 B = [0; 0; Km*Kg/(Mc*Rm*r); -Km*Kg/(r*Rm*Mc*l)];
 C = eye(4,4);
 D = zeros(4,1);
-system = ss(A,B,C,D);
-Q = [0.15 0 0 0; 0 4 0 0; 0 0 0 0; 0 0 0 0]; %hier mee spelen?
-R = 0.0003;
-K = lqr(system,Q,R);
+system_open = ss(A,B,C,D);
+Q = [0.25 0 0 0; 0 4 0 0; 0 0 0 0; 0 0 0 0]; %hier mee spelen?
+R = 0.03;
+K = lqr(system_open,Q,R);
 
 %ctrb
-Con = ctrb(system);
-RankConrollability=rank(Con) 
+Con = ctrb(system_open);
+RankControllability=rank(Con) 
 Con
 %observability
-Ob = obsv(system);
+Ob = obsv(system_open);
 RankObservability=rank(Ob)%rank 4 => not full rank => not observable
 Ob
 %stable
 eigenvalues = eig(A) %unstable: 0 6.0049 -5.0774 -17.8110
-eigenvalues_closed = eig (A-B*K)
+eigenvalues_closed = eig (A-B*K);
 %stabilizability
 
 %% evaluatie controller
-systeem = ss(A-B*K,B,C,D); %aanpassen van A en B
-pzmap(systeem);
-figure;
-impulse(systeem);
+
+A_closed=A-B*K
+intial_State= [0;0;0;0];
+reference_State = [1;0;0;0];
+system_Closed_First = ss(A_closed,B,C,D); %aanpassen van A en B
+%pzmap(system_Closed_First);
+%impulse(system_Closed_First);
 
 
 
